aa153d739eba6afce95663b494385838737fa10b
Steve White <stepwhite@gmail.com>
2017-02-16 18:03:54 -0800

    Add a slight delay between the open panel picking a file and loading it into the emulator, so that any alerts that may show don't end up hidden behind the open panel while simultaneously stealing focus from the open panel.

--- Leibniz/AppDelegate.m
+++ Leibniz/AppDelegate.m
@@ -123,9 +123,11 @@ void leibniz_undefined_opcode(newton_t *newton, uint32_t opcode);
 - (void) showOpenROMPanel {
   NSOpenPanel *openPanel = [NSOpenPanel openPanel];
   NSInteger result = [openPanel runModal];
+  
   if (result == NSFileHandlingPanelOKButton) {
     NSString *romFile = [[openPanel URL] path];
-    dispatch_async(dispatch_get_main_queue(), ^{
+
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
       [self beginEmulatingWithROMFile: romFile];
     });
   }
